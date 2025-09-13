c28ce00e6935a6d35bce174ac419b16e8b9c4b21
Guilherme Paiva <g.hapaiva@gmail.com>
2025-09-13 08:36:15 -0300

    refactor: added comments on methods of CRUD customer

--- lib/entities/customer/repository.py
+++ lib/entities/customer/repository.py
@@ -8,16 +8,52 @@ from lib.entities.customer.schema import CustomerBase, CustomerCreateBase
 
 
 class CRUDCustomer():
+    """
+    This class contains the methods that returns data from database
+    In a complete development, it would be good for this class to have an inheritance
+    """
+
     def get_all(self, db: Session) -> List[CustomerBase]:
+        """
+        Method that retrieves all the customers in database
+        Args:
+            The database session (db: Session)
+        Returns:
+            A list of customers (List[CustomerBase])
+        """
         return db.query(Customer).order_by(Customer.name).all()
 
     def get(self, db: Session, id: uuid.UUID) -> Optional[CustomerBase]:
+        """
+        Method that retrieves one customer based on the id
+        Args:
+            The database session (db: Session)
+            The customer id (id: uuid.UUID
+        Returns:
+            An optional customer schema (Optional[CustomerBase]]
+        """
         return db.query(Customer).filter(Customer.id == id).first()
 
     def get_by_email(self, db: Session, email: str) -> Optional[CustomerBase]:
+        """
+        Method that retrieves the customer based on e-mail
+        Args:
+            The database session (db: Session)
+            The e-mail that will be search (email: str)
+        Returns:
+            An optional customer schema (Optional[CustomerBase]]
+        """
         return db.query(Customer).filter(Customer.email == email).first()
 
     def create(self, db: Session, payload: CustomerCreateBase) -> CustomerBase:
+        """
+        Method that a new customer
+        Args:
+            The database session (db: Session)
+            The payload used to create the new user (payload: CustomerCreateBase)
+        Returns:
+            An customer schema (CustomerBase)
+        """
         try:
             db_customer: Customer = Customer(**payload.model_dump())
             db.add(db_customer)
@@ -29,6 +65,14 @@ class CRUDCustomer():
         return db_customer
 
     def update(self, db: Session, id: uuid.UUID, payload: CustomerCreateBase) -> CustomerBase:
+        """
+        Method that a updates customer
+        Args:
+            The database session (db: Session)
+            The payload used to update the user (payload: CustomerCreateBase)
+        Returns:
+            An customer schema (CustomerBase)
+        """
         try:
             db_customer: Customer = self.get(db=db, id=id)
             db_customer.name = payload.name
@@ -42,6 +86,14 @@ class CRUDCustomer():
         return db_customer
 
     def remove(self, db: Session, id: uuid.UUID) -> CustomerBase:
+        """
+        Method that a remove a customer from database
+        Args:
+            The database session (db: Session)
+            The customer id (id: uuid.UUID
+        Returns:
+            An customer schema (CustomerBase)
+        """
         try:
             db_customer: Customer = self.get(db=db, id=id)
             db.delete(db_customer)

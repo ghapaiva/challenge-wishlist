176d8143f85ef3348ec5a9d72377518f8d1d3edf
Guilherme Paiva <g.hapaiva@gmail.com>
2025-09-13 08:37:21 -0300

    refactor: change the product service by adding a class

--- lib/service/partner/products/repository.py
+++ lib/service/partner/products/repository.py
@@ -5,30 +5,34 @@ from typing import List, Optional
 from lib.service.partner.products.schema import ProductBase
 
 
-def get_all() -> List[ProductBase]:
+class PartnerProductService():
     """
-    Return all products from partner
-    This method is mocked. When the API is done, this method must be replaced by using requests lib to access API
-    Returns:
-        A list of product base (List[ProductBase])
+    Class that communicates with partner to retrieve the products
     """
-    PRODUCTS_FILE = "/home/app/products.json"
-    with open(PRODUCTS_FILE, "r", encoding="utf-8") as f:
-        products = json.load(f)
-    return products
 
+    def get_all(self) -> List[ProductBase]:
+        """
+        Return all products from partner
+        This method is mocked. When the API is done, this method must be replaced by using requests lib to access API
+        Returns:
+            A list of product base (List[ProductBase])
+        """
+        PRODUCTS_FILE = "/home/app/products.json"
+        with open(PRODUCTS_FILE, "r", encoding="utf-8") as f:
+            products = json.load(f)
+        return products
 
-def get(id: uuid.UUID) -> Optional[ProductBase]:
-    """
-    Return the product by the id
-    This method is mocked. When the API is done, this method must be replaced by using requests lib to access API
-    Args:
-        Product primary key (id: uuid.UUID)
-    Returns:
-        An optional product schema (Optional[ProductBase])
-    """
-    PRODUCTS_FILE = "/home/app/products.json"
-    with open(PRODUCTS_FILE, "r", encoding="utf-8") as f:
-        products = json.load(f)
-    product = next((row for row in products if row["id"] == id), None)
-    return product
+    def get(self, id: uuid.UUID) -> Optional[ProductBase]:
+        """
+        Return the product by the id
+        This method is mocked. When the API is done, this method must be replaced by using requests lib to access API
+        Args:
+            Product primary key (id: uuid.UUID)
+        Returns:
+            An optional product schema (Optional[ProductBase])
+        """
+        PRODUCTS_FILE = "/home/app/products.json"
+        with open(PRODUCTS_FILE, "r", encoding="utf-8") as f:
+            products = json.load(f)
+        product = next((row for row in products if row["id"] == str(id)), None)
+        return product
